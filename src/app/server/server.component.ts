8576bc81ccb90cbfaae3c402222a66bc6dda5484
Andrés <ernestoandresrosillo@gmail.com>
2024-02-29 17:01:07 +0100

    ![COMPONENT] SERVER COMPONENT!

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    // standalone: true, // we don't put it in ngmodule
+    selector: 'app-server', // unique selector
+    templateUrl: './server.component.html',
+    // template: '<h1>I work standalone</h1>', // and we remove the templateUrl
+    // imports: [OtherComponent], // to tell angular which features can have our standalone component
+
+})
+export class ServerComponent {
+
+}
