7064fb4adffcc23250585980a36a62ee8524e42a
Andrés <ernestoandresrosillo@gmail.com>
2024-02-29 23:39:13 +0100

    [DATA BINDING] ONE WAY, PROP AND INTERPOLATION

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,5 +9,8 @@ import { Component } from "@angular/core";
 
 })
 export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
+    getServerStatus = () => this.serverStatus;
 }
