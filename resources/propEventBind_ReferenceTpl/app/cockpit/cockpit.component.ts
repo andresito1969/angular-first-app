bb9dedaab28ced35f712942cfb10a2fd883ad843
Andrés <ernestoandresrosillo@gmail.com>
2024-03-03 01:26:53 +0100

    [important] reference binding in template only v2, with ViewChild decorator

--- resources/propEventBind_ReferenceTpl/app/cockpit/cockpit.component.ts
+++ resources/propEventBind_ReferenceTpl/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,20 +8,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent {
   @Output() childAddServer = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() childAddBlueprint = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInp') serverContentInp: ElementRef;
 
   onAddServer(serverName : HTMLInputElement) {
     this.childAddServer.emit({
       serverName: serverName.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInp.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(serverName : HTMLInputElement) {
     this.childAddBlueprint.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: serverName.value,
+      serverContent: this.serverContentInp.nativeElement.value
     });
   }
 }
